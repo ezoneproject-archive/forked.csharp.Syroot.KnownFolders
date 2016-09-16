9833fa83c711ba030fbacbef40fcc270cd51dce8
Ray Koopa <raykoopa@users.noreply.github.com>
2016-09-16 18:39:07 +0200

    Added support for .NET 3.5 Client Profile

--- src/Syroot.Windows.IO.KnownFolders/Properties/AssemblyInfo.cs
+++ src/Syroot.Windows.IO.KnownFolders/Properties/AssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("WTFPL")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("E61E05F2-AEAB-41DB-958F-1398D6BFFD3F")]
--- src/Tools/Build NuGet.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-:: Make sure that the NuGet command line executable is in your PATH or edit this batch file.
-NUGET pack ..\Syroot.Windows.IO.KnownFolders\Syroot.Windows.IO.KnownFolders.csproj -Prop Configuration=Release -Build
